83818d217b98cf7fb03f36a971b1bda40f0ce4e0
VasuKakadiya20 <vasukakadiya1234@gmail.com>
2025-11-26 11:28:40 +0530

    api

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchDataFromApi = async (url) => {
   try {
-    const { data } = await axios.get("http://localhost:4000" + url);
+    const { data } = await axios.get("https://shop-server-btcc.onrender.com" + url);
     return data;
   } catch (error) {
     console.log(error);
@@ -13,7 +13,7 @@ export const fetchDataFromApi = async (url) => {
 
 export const postData = async (url, fromdata) => {
   try {
-    const { data } = await axios.post("http://localhost:4000" + url, fromdata);
+    const { data } = await axios.post("https://shop-server-btcc.onrender.com" + url, fromdata);
     return data;
   } catch (error) {
     console.error("POST Error:", error.response?.data || error.message);
@@ -24,10 +24,13 @@ export const postData = async (url, fromdata) => {
 
 export const Deletedata = async (url) => {
   try{
-    const { data } = await axios.delete(`http://localhost:4000${url}`);
+    const { data } = await axios.delete(`https://shop-server-btcc.onrender.com${url}`);
     return data
   }catch (error) {
     console.error ("Delete Error:-", error.response?.data || error.message);
     throw error
   }
 }
+
+// http://localhost:4000
+// https://shop-server-btcc.onrender.com
